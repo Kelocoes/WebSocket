5a424779eb773a1476d7f11c6efe80da66fa10d5
kelocoes <kevin.rodriguez109@gmail.com>
2024-11-04 16:44:04 -0500

    Add useMemo and useCallback

--- front/src/pages/ChatPage.jsx
+++ front/src/pages/ChatPage.jsx
@@ -1,5 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 import stompService from "../utils/StompService";
+
+const expensiveFunction = (field) => {
+    console.log('I am validating the field');
+    //Expensive function that validates the field
+    for(let i = 0; i < 1000000000; i++){
+        //do nothing
+    }
+    return true;
+}
+
+const debounce = (func, wait) =>  {
+    let timeout = null;
+    console.log("Outside", timeout, wait, func);
+    return function (...args) {
+        console.log("Inside", timeout);
+        const later = () => {
+            timeout = null;
+            func(...args);
+        };
+        if (timeout !== null) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(later, wait);
+    };
+}
+
+
 
 export default function ChatPage () {
     const user = localStorage.getItem('user');
@@ -8,6 +35,16 @@ export default function ChatPage () {
     const [ actualUser, setActualUser] = useState(user?.toLowerCase());
     const [ message, setMessage] = useState("");
     const [ to, setTo] = useState("");
+    const [darkMode, setDarkMode] = useState(true);
+
+    // eslint-disable-next-line
+    const expensiveFunctionWithCallback = useCallback(debounce(() => expensiveFunction(message), 1000), []);
+    // eslint-disable-next-line
+    const isValidated = useMemo(() => expensiveFunctionWithCallback(message), [message]);
+    const myStyle = {
+        background: darkMode ? 'black' : 'white',
+        color: darkMode ? 'white' : 'black'
+    }
 
     const sendMessage = () => {
         stompService.publish("/message", {
@@ -49,8 +86,10 @@ export default function ChatPage () {
     }
 
     return (
-        <div style={{ width: '80%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column' }}>
+        <div style={{ width: '80%', display: 'flex', alignItems: 'center', flexDirection:'column', ...myStyle, height: '100vh'}} >
             <h1>Chat</h1>
+            <button onClick={() => setDarkMode(prev => !prev)}>Dark Mode: {darkMode.toString()}</button>
+            <p>{isValidated ? 'Valid!' : 'Not valid'}</p>
             <div id="chat">
                 <input type="text" placeholder="To" onChange={(e) => setTo(e.target.value)} />
                 <input type="text" placeholder="Message" onChange={(e) => setMessage(e.target.value)} />
