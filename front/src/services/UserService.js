683a9781d69a678224cab714142370cf3c2dd311
kelocoes <kevin.rodriguez109@gmail.com>
2024-11-04 15:26:51 -0500

    Axios

--- front/src/services/UserService.js
+++ front/src/services/UserService.js
@@ -1,10 +1,44 @@
 import { axiosInstance } from "./axios";
+import axios from "axios";
 
-export function getUserById () {
-    return axiosInstance.get('/api/user/1')
+export function getUserById (id) {
+    return axiosInstance.get(`/api/user/${id}`)
         .then(response => {
-            console.log(response);
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+            console.log(response)
             return response.data
         })
         .catch(error => error);
+}
+
+export function createUserWithApi () {
+    return axios({
+        method: 'post',
+        url: 'http://localhost:8080/api/user',
+        data: {
+            name: 'test',
+            email: 'test@test.com'
+        }
+    }).then(response => {
+        console.log(response);
+        return response.data;
+    }).catch(error => {
+        console.error(error);
+    });
+}
+
+export function createUserWithInstance () {
+    return axiosInstance.post('/api/user', {
+        name: 'test',
+        email: 'test@test.com'
+    }).then(response => {
+        console.log(response);
+        return response.data;
+    }).catch(error => {
+        console.error(error);
+    });
 }
